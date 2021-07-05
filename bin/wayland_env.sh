8643a07b459026de4a2c9327fd8a0759fddb4d06
Dinesh Natesan <AbstractGeek@users.noreply.github.com>
2021-07-05 13:11:47 +0530

    modified borgmatic settings, way shortcuts, and minor customizations

--- bin/wayland_env.sh
+++ bin/wayland_env.sh
@@ -1,6 +1,9 @@
 # Firefox doesn't use wayland by default yet:
 export MOZ_ENABLE_WAYLAND=1
+export MOZ_DBUS_REMOTE=1
 # Older versions of Qt always show window decorations. To hide them:
 export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
 # Some Java AWT apps would not display properly unless you set the following:
 export _JAVA_AWT_WM_NONREPARENTING=1
+eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
+export SSH_AUTH_SOCK
